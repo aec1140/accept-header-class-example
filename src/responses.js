47c36ee4c88c58663fb250e20df2853ea09f884e
Cody Van De Mark <cavigm@rit.edu>
2017-02-07 01:43:32 -0500

    initial commit

--- src/responses.js
+++ src/responses.js
@@ -2,14 +2,14 @@ const fs = require('fs');  // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
-const respondHTML = (request, response, html) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(html);
+const respond = (request, response, content, type) => {
+  response.writeHead(200, { 'Content-Type': type });
+  response.write(content);
   response.end();
 };
 
 const getIndex = (request, response) => {
-  respondHTML(request, response, index);
+  respond(request, response, index, 'text/html');
 };
 
 module.exports = {
